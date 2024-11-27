462692bff090daedcb8673bb8d8fac1899245f5a
jpmedia-29 <maruf.rembang29@gmail.com>
2024-11-27 20:02:52 +0700

    set up projek by maruf

---
+++ resources/views/admin/profil.blade.php
@@ -0,0 +1,40 @@
+@extends('template/admin')
+@section('title','Halaman Profil')
+@section('page_name','Halaman Profil')
+@section('data_name','Data Profil')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Data Profil</h3>
+    </div>
+        <div class="card-body">
+           <form method="POST" action="" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="nama_instansi">Nama Instansi</label>
+                <input type="text" name="nama_instansi" value="" class="form-control" required>
+            </div> 
+            
+            <div class="form-group">
+                <label for="logo">Logo</label>
+                <input type="file" name="logo" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <textarea name="alamat" class="form-control" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="no_telp">No Telp</label>
+                <input type="number" name="no_telp" class="form-control" value="" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" name="email" value="" class="form-control" required>
+            </div> 
+            <button type="submit" class="btn btn-primary float-right">Simpan</button>
+         </form>
+        </div>
+    
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\AboutController;
+use App\Http\Controllers\Admin\KategoriController;
+use App\Http\Controllers\Admin\MateriController;
+use App\Http\Controllers\Admin\ModulController;
+use App\Http\Controllers\Admin\ProfilController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/login',[LoginController::class,'index']);
+Route::get('/profil',[ProfilController::class,'index']);
+Route::get('/about',[AboutController::class,'index']);
+Route::get('/kategori',[KategoriController::class,'index']);
+Route::get('/create_kategori',[KategoriController::class,'create_kategori']);
+Route::get('/modul',[ModulController::class,'index']);
+Route::get('/create_modul',[ModulController::class,'create_modul']);
+Route::get('/materi',[MateriController::class,'index']);
+Route::get('/create_materi',[MateriController::class,'create_materi']);
