78eee1cd3e82535c90e5b188af03b05116b85f62
Alii354 <Aliimronn354@gmail.com>
2024-11-27 22:13:54 +0700

    membuat tampilan edit modul

---
+++ resources/views/admin/edit_modul.blade.php
@@ -0,0 +1,28 @@
+@extends('template/admin')
+@section('title','Halaman Edit Modul')
+@section('page_name','Halaman Edit Modul')
+@section('data_name','Data Kategori')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Edit Modul</h3>
+    </div>
+        <div class="card-body">
+           <form method="POST" action="" enctype="multipart/form-data">
+            @csrf
+           
+            
+            <div class="form-group">
+                <label for="nama_kategori">Nama Modul</label>
+                <input type="text" name="nama_kategori" class="form-control" required>
+            </div>
+     
+          
+         
+            <button type="submit" class="btn btn-primary float-right">Simpan</button>
+         </form>
+        </div>
+    
+</div>
+@endsection
