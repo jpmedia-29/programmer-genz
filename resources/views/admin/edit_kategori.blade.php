170c1549dcd37b958a6dfb390c9100ddebad388d
Alii354 <Aliimronn354@gmail.com>
2024-12-04 20:42:15 +0700

    membuat tampilan edit kategori

---
+++ resources/views/admin/edit_kategori.blade.php
@@ -0,0 +1,28 @@
+@extends('template/admin')
+@section('title','Halaman Edit Kategori')
+@section('page_name','Halaman Edit Katgeori')
+@section('data_name','Data Kategori')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Edit Kategori</h3>
+    </div>
+        <div class="card-body">
+           <form method="POST" action="" enctype="multipart/form-data">
+            @csrf
+           
+            
+            <div class="form-group">
+                <label for="nama_kategori">Nama Kategori</label>
+                <input type="text" name="nama_kategori" class="form-control" required>
+            </div>
+     
+          
+         
+            <button type="submit" class="btn btn-primary float-right">Simpan</button>
+         </form>
+        </div>
+    
+</div>
+@endsection
